772bbd10439af7c6fab44685a2cbad9e591e6050
nattapol pandokmai <nattapolbb@gmail.com>
2015-05-13 20:07:37 +0700

    Initial commit

---
+++ app/src/main/java/rmuti/sensors3/MainActivity.java
@@ -0,0 +1,74 @@
+package rmuti.sensors3;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity implements SensorEventListener {
+    int xx = 0;
+    private SensorManager sensorManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor mySensor = event.sensor;
+        TextView test = (TextView)findViewById(R.id.textView);
+        TextView test2 = (TextView)findViewById(R.id.textView2);
+        if (mySensor.getType() == Sensor.TYPE_LIGHT) {
+            float x = event.values[0];
+            Log.v("test", "x=" + x);
+            if(x < 100){
+                test.setText("ไม่ควรอ่านหนังสือ");
+            }
+            else if(x < 400){
+                test.setText("อ่านหนังสือได้");
+            }
+            else{
+                test.setText("แสงสว่างเกินไป");
+            }
+            test2.setText("Sensor LIGHT = " + x);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
